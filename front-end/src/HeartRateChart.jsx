75ac2c09e9a062f2e50f3ddff01d4c1a1be259cd
oussamahamani <oussamahamani2000@gmail.com>
2025-05-02 15:33:47 -0400

    adding average beats and showing last updated and formatting time better with dayjs

--- front-end/src/HeartRateChart.jsx
+++ front-end/src/HeartRateChart.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
-import styles from "./HeartRateChart.module.css"; // Assuming you have some styles in HeartRateChart.module.css
+import styles from "./HeartRateChart.module.css"; 
+import dayjs from "dayjs";
+
 const HeartRateChart = ({patientData}) => {
 
   const times = patientData.times.map((time) => new Date(time))
   const bpmValues = patientData.bpmValues
 
+  const bpm = Math.floor(bpmValues.reduce((a, b) => a + b) / bpmValues.length);
+
   return (
     <div className={styles.chartContainer}>
-      <LineChart
-        yAxis={[
-          { label: "Heart Rate (BPM)", scaleType: "linear", min: 40, max: 100 },
-        ]}
-        xAxis={[{ data: times, scaleType: "time", label: "Time" }]}
-        series={[
-          { data: bpmValues, label: "Heart Rate Monitor", showMark: false },
-        ]}
-        height={400}
+        <div className={styles.bpm}>
+        {bpm} BPM
+      </div>
+      
 
-      />
+<LineChart
+  yAxis={[
+    { label: "Heart Rate (BPM)", scaleType: "linear", min: 40, max: 100 },
+  ]}
+  xAxis={[
+    { 
+      data: times, 
+      scaleType: "time", 
+      label: "Time",
+      valueFormatter: (value) => dayjs(value).format("H:mm:ss")
+    }
+  ]}
+  series={[
+    { data: bpmValues, label: "Heart Rate Monitor", showMark: false },
+  ]}
+  height={400}
+/>
+
+
+<div className={styles.lastUpdated}>
+        Live Heart Rate Monitoring - Last Updated: {new Date().toLocaleTimeString()}
+      </div>
+
     </div>
   );
 };
