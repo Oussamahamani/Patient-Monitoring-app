0216fcbf0283a91eeba98624723c01f14d4eabdf
oussamahamani <oussamahamani2000@gmail.com>
2025-05-01 23:18:26 -0400

    creating home and navbar components and adding mui icons

---
+++ frontEnd/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './NavBar.jsx'
+import styles from './Home.module.css'; // Assuming you have some styles in Home.module.css
+const Home = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    );
+}
+
+export default Home;
