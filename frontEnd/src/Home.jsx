39a070bcbfce9bcb3f40b5f465410b1ab3ec5022
oussamahamani <oussamahamani2000@gmail.com>
2025-05-01 23:20:06 -0400

    removing default project styling and using appBar from mui

--- frontEnd/src/Home.jsx
+++ frontEnd/src/Home.jsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar.jsx'
 import styles from './Home.module.css'; // Assuming you have some styles in Home.module.css
 const Home = () => {
     return (
-        <div>
+        <div className={styles.homeContainer}>
             <NavBar/>
         </div>
     );
