e6efec0fdaa8388c9e666b7e0a640bad7941e1bb
oussamahamani <oussamahamani2000@gmail.com>
2025-05-01 23:34:33 -0400

    adding line chart using mui charts to represent patient heart rate

--- frontEnd/src/Home.jsx
+++ frontEnd/src/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar.jsx'
 import styles from './Home.module.css'; // Assuming you have some styles in Home.module.css
+import HeartRateChart from './HeartRateChart.jsx';
 const Home = () => {
     return (
         <div className={styles.homeContainer}>
             <NavBar/>
+            <HeartRateChart/>
         </div>
     );
 }
